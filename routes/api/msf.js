dcb6da44bfafbad6275fc7bdcac83d5081343bc5
john horta <john.horta@yahoo.com>
2019-09-03 23:20:08 -0400

    app change

--- routes/api/msf.js
+++ routes/api/msf.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const msf = require("../../sportfeed/sportsData");
+// const msf = require("../../sportfeed/sportsData");
 const axios = require("axios");
 const btoa = (str) => new Buffer(str).toString('base64');
 
